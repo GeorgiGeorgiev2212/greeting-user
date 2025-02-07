02a114fb7029362886f24fddc52bde850fde47b6
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:30:38 +0200

    Add project files.

---
+++ greeting user/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Type your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Welcome, {name}, to Whiz Academy for C#!");
+    }
+}
+
+
+
+
